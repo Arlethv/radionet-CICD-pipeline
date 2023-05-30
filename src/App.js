2f8dd63965f7a9e334d0042070a987dfb195cab6
Arlethv <yoselynvaquedanog@gmail.com>
2023-05-29 18:36:20 -0600

    RadioNet App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import React from 'react';
+import RadioNetImage from './assets/img/RadioNet.svg';
+import escuchandoMusica from './assets/img/escuchando-musica.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className="masthead d-flex align-items-center" style={{ backgroundImage: `url(${RadioNetImage})` }}>
+        <div className="container px-4 px-lg-5 text-center">
+          <h1 className="mb-1">RadioNet</h1>
+          <h3 className="mb-5"><em>Conéctate con la música que te mueve</em></h3>
+          <a className="btn btn-primary btn-xl">Escucha ahora</a>
+        </div>
       </header>
+      <section className="content-section bg-light" id="about">
+        <div className="container px-4 px-lg-5 text-center">
+          <div className="row gx-4 gx-lg-5 justify-content-center">
+            <div className="col-lg-10">
+              <h2>RadioNet!</h2>
+              <p className="lead mb-5"></p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section>
+        <img src={escuchandoMusica} alt="Descripción de la imagen" style={{ width: '100%' }} />
+      </section>
+      <section className="content-section bg-primary text-white">
+        <div className="container px-4 px-lg-5 text-center">
+          <h2 className="mb-4">RadioNet: La cautivadora melodía que no podrás resistir...</h2>
+        </div>
+      </section>
+      <section className="py-5">
+        <div className="container px-5">
+          {/* Resto del código */}
+        </div>
+      </section>
+      <footer className="footer text-center">
+        <div className="container px-4 px-lg-5">
+          {/* Resto del código */}
+        </div>
+      </footer>
+      <a className="scroll-to-top rounded" href="#page-top"><i className="fas fa-angle-up"></i></a>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,41 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders RadioNet text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const textElement = screen.getByText('RadioNet');
+  expect(textElement).toBeInTheDocument();
 });
+
+function validateFields(name, email, phone, message) {
+  if (name === '') {
+    return 'El campo de nombre es obligatorio';
+  }
+
+  if (email === '') {
+    return 'El campo de correo electrónico es obligatorio';
+  }
+
+  return null;
+}
+
+describe('Prueba de validación de campos', () => {
+  it('Debe retornar un mensaje de error si el campo de nombre está vacío', () => {
+    // Ejecutar la función de validación
+    const errorMessage = validateFields('', 'test@example.com', '1234567890', 'Mensaje de prueba');
+
+    // Verificar si se retorna el mensaje de error esperado
+    expect(errorMessage).toBe('El campo de nombre es obligatorio');
+  });
+
+  it('Debe retornar un mensaje de error si el campo de correo electrónico está vacío', () => {
+    // Ejecutar la función de validación
+    const errorMessage = validateFields('Nombre', '', '1234567890', 'Mensaje de prueba');
+
+    // Verificar si se retorna el mensaje de error esperado
+    expect(errorMessage).toBe('El campo de correo electrónico es obligatorio');
+  });
+
+  // Agrega más pruebas según sea necesario
+});
+
