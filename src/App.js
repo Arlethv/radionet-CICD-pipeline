fb0fa183d4f6592e268b7b0bbf8913e126333862
Arlethv <yoselynvaquedanog@gmail.com>
2023-06-13 19:14:14 -0600

    Prueba Calidad de Codigo

--- src/App.js
+++ src/App.js
@@ -1,132 +1,140 @@
-
-import { useForm } from 'react-hook-form';
-import 'bootstrap/dist/css/bootstrap.css';
-import React from 'react';
-import RadioNetImage from './assets/img/RadioNet.svg';
-import escuchandoMusica from './assets/img/escuchando-musica.svg';
-import { FaFacebook, FaTwitter, FaGithub } from 'react-icons/fa';
-import './App.css';
+import { useForm } from "react-hook-form";
+import "bootstrap/dist/css/bootstrap.css";
+import React from "react";
+import RadioNetImage from "./assets/img/RadioNet.svg";
+import escuchandoMusica from "./assets/img/escuchando-musica.svg";
+import { FaFacebook, FaTwitter, FaGithub } from "react-icons/fa";
+import "./App.css";
 
 function App() {
-
-
   const {
     register,
     handleSubmit,
-    formState: { errors},
-    trigger
+    formState: { errors },
+    trigger,
   } = useForm();
 
   const onSubmit = (data) => {
-  console.log(data);
-};
+    console.log(data);
+  };
 
   const validacionNombre = (value) => {
     // Expresión regular para permitir solo letras y espacios
     const regex = /^[A-Za-z\s]+$/;
-    return regex.test(value) || 'El nombre debe contener solo letras.';
+    return regex.test(value) || "El nombre debe contener solo letras.";
   };
 
   const validacionCorreo = (value) => {
     // Expresión regular para validar el formato de correo electrónico
     const regex = /^\S+@\S+\.[A-Za-z]+$/;
-    return regex.test(value) || 'El correo electrónico no es válido.';
+    return regex.test(value) || "El correo electrónico no es válido.";
   };
 
   const validacionTelefono = (value) => {
     const regex = /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s./0-9]*$/;
-    return regex.test(value) || 'El número de teléfono no es válido.';
+    return regex.test(value) || "El número de teléfono no es válido.";
   };
-  
-  
-
 
   return (
     <div id="inicio">
       <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-          <div className="container">
-            <a className="navbar-brand text-white" href="#inicio">
-              RadioNet
-            </a>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarNav"
-              aria-controls="navbarNav"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-              <ul className="navbar-nav ms-auto">
-                <li className="nav-item">
-                  <a className="nav-link text-white" href="#inicio">
-                    Inicio
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link text-white" href="#acerca">
-                    Acerca
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link text-white" href="#servicios">
-                    Servicios
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link text-white" href="#contacto">
-                    Contacto
-                  </a>
-                </li>
-              </ul>
-            </div>
+        <div className="container">
+          <a className="navbar-brand text-white" href="#inicio">
+            RadioNet
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <a className="nav-link text-white" href="#inicio">
+                  Inicio
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link text-white" href="#acerca">
+                  Acerca
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link text-white" href="#servicios">
+                  Servicios
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link text-white" href="#contacto">
+                  Contacto
+                </a>
+              </li>
+            </ul>
           </div>
-        </nav>
-        
+        </div>
+      </nav>
+
       <header
         className="d-flex align-items-center"
         style={{
           backgroundImage: `url(${RadioNetImage})`,
-          backgroundSize: 'cover',
-          backgroundRepeat: 'no-repeat',
-          backgroundAttachment: 'flex',
-          minHeight: '100vh'
-        }}>
-          
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          backgroundAttachment: "flex",
+          minHeight: "100vh",
+        }}
+      >
         <div className="container px-4 px-lg-5 text-center">
           <h1 className="col-lg-12 text-white">RadioNet: </h1>
-          <h3 >
-            <em className='bg-dark'>Conéctate con la música que te mueve</em>
+          <h3>
+            <em className="bg-dark">Conéctate con la música que te mueve</em>
           </h3>
-          <a className="btn btn-primary btn-xl" href='#contacto'>Escucha ahora</a>
+          <a className="btn btn-primary btn-xl" href="#contacto">
+            Escucha ahora
+          </a>
         </div>
       </header>
 
-      <section id="acerca" className='bg-light'> 
+      <section id="acerca" className="bg-light">
         <div className="container-fluid px-0 text-center">
           <div className="row justify-content-center">
             <div className="col-lg-12 p-0">
               <div id="info1" className="bg-light  py-5 my-0">
                 <h2 className="mb-4">RadioNet:</h2>
-                <p className="radio mb-5" >RadioNet es una estación innovadora y joven. Cuenta solo con doce empleados. Si bien
-comenzó operaciones en Silao, tiene como misión cubrir zonas en el Bajío, donde la señal es
-débil, y como meta, la expansión de sus servicios para brindar comunicación por Internet.</p>
+                <p className="radio mb-5">
+                  RadioNet es una estación innovadora y joven. Cuenta solo con
+                  doce empleados. Si bien comenzó operaciones en Silao, tiene
+                  como misión cubrir zonas en el Bajío, donde la señal es débil,
+                  y como meta, la expansión de sus servicios para brindar
+                  comunicación por Internet.
+                </p>
               </div>
             </div>
           </div>
         </div>
-    </section>
+      </section>
 
-      <section id="servicios"> 
-      <img src={escuchandoMusica} alt="Descripción de la imagen" style={{ width: '100%' }} />
-      <div id="info2" className="container-fluid text-white bg-dark py-5 my-5">
-        <h2 className="mb-4 text-center">RadioNet: La cautivadora melodía que no podrás resistir...</h2>
-      </div>
-  </section>
-
+      <section id="servicios">
+        <img
+          src={escuchandoMusica}
+          alt="Descripción de la imagen"
+          style={{ width: "100%" }}
+        />
+        <div
+          id="info2"
+          className="container-fluid text-white bg-dark py-5 my-5"
+        >
+          <h2 className="mb-4 text-center">
+            RadioNet: La cautivadora melodía que no podrás resistir...
+          </h2>
+        </div>
+      </section>
 
       <section id="contacto" className="py-5">
         <div className="container px-5">
@@ -136,117 +144,161 @@ débil, y como meta, la expansión de sus servicios para brindar comunicación p
                 <h1>R</h1>
               </div>
               <h1 className="fw-bolder">Suscríbete</h1>
-              <p className="lead fw-normal text-muted mb-0">¡Disfruta de tu música!</p>
+              <p className="lead fw-normal text-muted mb-0">
+                ¡Disfruta de tu música!
+              </p>
             </div>
             <div className="row gx-5 justify-content-center">
               <div className="col-lg-8 col-xl-6">
-              <form id="contactForm" onSubmit={handleSubmit(onSubmit)}>
-      <div className="form-floating mb-3">
-        <input
-          className={`form-control ${errors.name ? 'is-invalid' : ''}`}
-          {...register('name', { required: 'El nombre es obligatorio', validate: validacionNombre })}
-          id="name"
-          type="text"
-          placeholder="Ingrese su nombre completo"
-          onBlur={() => {
-            trigger('name');
-          }}
-        />
-        <label htmlFor="name">Nombre Completo</label>
-        {errors.name && (
-          <div className="invalid-feedback">{errors.name.message}</div>
-        )}
-        {!errors.name && (
-          <div className="valid-feedback">El nombre es válido.</div>
-        )}
-      </div>
+                <form id="contactForm" onSubmit={handleSubmit(onSubmit)}>
+                  <div className="form-floating mb-3">
+                    <input
+                      className={`form-control ${
+                        errors.name ? "is-invalid" : ""
+                      }`}
+                      {...register("name", {
+                        required: "El nombre es obligatorio",
+                        validate: validacionNombre,
+                      })}
+                      id="name"
+                      type="text"
+                      placeholder="Ingrese su nombre completo"
+                      onBlur={() => {
+                        trigger("name");
+                      }}
+                    />
+                    <label htmlFor="name">Nombre Completo</label>
+                    {errors.name && (
+                      <div className="invalid-feedback">
+                        {errors.name.message}
+                      </div>
+                    )}
+                    {!errors.name && (
+                      <div className="valid-feedback">El nombre es válido.</div>
+                    )}
+                  </div>
 
-      <div className="form-floating mb-3">
-        <input
-          className={`form-control ${errors.email ? 'is-invalid' : ''}`}
-          {...register('email', { required: 'El correo electrónico es obligatorio', validate: validacionCorreo })}
-          id="email"
-          type="email"
-          placeholder="nombre@ejemplo.com"
-          onBlur={() => {
-            // Validar el campo al perder el foco
-            trigger('email');
-          }}
-        />
-        <label htmlFor="email">Correo Electrónico:</label>
-        {errors.email && (
-          <div className="invalid-feedback">{errors.email.message}</div>
-        )}
-        {!errors.email && (
-          <div className="valid-feedback">El correo electrónico es válido.</div>
-        )}
-      </div>
+                  <div className="form-floating mb-3">
+                    <input
+                      className={`form-control ${
+                        errors.email ? "is-invalid" : ""
+                      }`}
+                      {...register("email", {
+                        required: "El correo electrónico es obligatorio",
+                        validate: validacionCorreo,
+                      })}
+                      id="email"
+                      type="email"
+                      placeholder="nombre@ejemplo.com"
+                      onBlur={() => {
+                        trigger("email");
+                      }}
+                    />
+                    <label htmlFor="email">Correo Electrónico:</label>
+                    {errors.email && (
+                      <div className="invalid-feedback">
+                        {errors.email.message}
+                      </div>
+                    )}
+                    {!errors.email && (
+                      <div className="valid-feedback">
+                        El correo electrónico es válido.
+                      </div>
+                    )}
+                  </div>
 
-      <div className="form-floating mb-3">
-        <input
-          className={`form-control ${errors.telefono ? 'is-invalid' : ''}`}
-          {...register('telefono', { required: 'El número de teléfono es obligatorio', validate: validacionTelefono })}
-          id="telefono"
-          type="tel"
-          placeholder="Ingresa tu teléfono"
-          onBlur={() => {
-            // Validar el campo telefono al ingresar datos
-            trigger('telefono');
-          }}
-        />
-        <label htmlFor="telefono">Teléfono:</label>
-        {errors.telefono && (
-          <div className="invalid-feedback">{errors.telefono.message}</div>
-        )}
-        {!errors.telefono && (
-          <div className="valid-feedback">El teléfono es válido.</div>
-        )}
-      </div>
+                  <div className="form-floating mb-3">
+                    <input
+                      className={`form-control ${
+                        errors.telefono ? "is-invalid" : ""
+                      }`}
+                      {...register("telefono", {
+                        required: "El número de teléfono es obligatorio",
+                        validate: validacionTelefono,
+                      })}
+                      id="telefono"
+                      type="tel"
+                      placeholder="Ingresa tu teléfono"
+                      onBlur={() => {
+                        trigger("telefono");
+                      }}
+                    />
+                    <label htmlFor="telefono">Teléfono:</label>
+                    {errors.telefono && (
+                      <div className="invalid-feedback">
+                        {errors.telefono.message}
+                      </div>
+                    )}
+                    {!errors.telefono && (
+                      <div className="valid-feedback">
+                        El teléfono es válido.
+                      </div>
+                    )}
+                  </div>
 
-      <div className="form-floating mb-3">
-        <textarea
-          className="form-control"
-          id="mensaje"
-          type="text"
-          placeholder="Ingrese su mensaje aquí..."
-          style={{ height: '10rem' }} 
-        ></textarea>
-        <label htmlFor="mensaje">Mensaje:</label>
-      </div>
-      <div className="d-grid">
-        <button className="btn btn-primary btn-lg" id="submitButton" type="submit">
-          Enviar
-        </button>
-      </div>
-    </form>
+                  <div className="form-floating mb-3">
+                    <textarea
+                      className="form-control"
+                      id="mensaje"
+                      type="text"
+                      placeholder="Ingrese su mensaje aquí..."
+                      style={{ height: "10rem" }}
+                    ></textarea>
+                    <label htmlFor="mensaje">Mensaje:</label>
+                  </div>
+                  <div className="d-grid">
+                    <button
+                      className="btn btn-primary btn-lg"
+                      id="submitButton"
+                      type="submit"
+                    >
+                      Enviar
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  </div>
-</section>
+      </section>
 
       <footer className="footer text-center">
-      <div className="container px-4 px-lg-5">
-        <ul className="list-inline mb-5">
-          <li className="list-inline-item">
-            <a className="social-link rounded-circle text-white mr-3" href="https://www.facebook.com/" target="_blank" rel="noreferrer">
-              <FaFacebook />
-            </a>
-          </li>
-          <li className="list-inline-item">
-            <a className="social-link rounded-circle text-white mr-3" href="https://www.twitter.com/" target="_blank" rel="noreferrer">
-              <FaTwitter />
-            </a>
-          </li>
-          <li className="list-inline-item">
-            <a className="social-link rounded-circle text-white"  href="https://www.github.com/ ">
-              <FaGithub />
-            </a>
-          </li>
-        </ul>
-      </div>
-    </footer>
-      <a className="scroll-to-top rounded" href="#page-top"><i className="fas fa-angle-up"></i></a>
+        <div className="container px-4 px-lg-5">
+          <ul className="list-inline mb-5">
+            <li className="list-inline-item">
+              <a
+                className="social-link rounded-circle text-white mr-3"
+                href="https://www.facebook.com/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaFacebook />
+              </a>
+            </li>
+            <li className="list-inline-item">
+              <a
+                className="social-link rounded-circle text-white mr-3"
+                href="https://www.twitter.com/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaTwitter />
+              </a>
+            </li>
+            <li className="list-inline-item">
+              <a
+                className="social-link rounded-circle text-white"
+                href="https://www.github.com/ "
+              >
+                <FaGithub />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </footer>
+      <a className="scroll-to-top rounded" href="#page-top">
+        <i className="fas fa-angle-up"></i>
+      </a>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,44 +1,60 @@
-import React from 'react';
-import { render, screen, fireEvent} from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-describe('App', () => {
-  it('Muestra los campos del formulario', () => {
+describe("App", () => {
+  it("Muestra los campos del formulario", () => {
     render(<App />);
-    
+
     // Verificar que los campos de formulario estén presentes
-    expect(screen.getByLabelText('Nombre Completo')).toBeInTheDocument();
-    expect(screen.getByLabelText('Correo Electrónico:')).toBeInTheDocument();
-    expect(screen.getByLabelText('Teléfono:')).toBeInTheDocument();
-    expect(screen.getByLabelText('Mensaje:')).toBeInTheDocument();
-    expect(screen.getByRole('button', { name: 'Enviar' })).toBeInTheDocument();
+    expect(screen.getByLabelText("Nombre Completo")).toBeInTheDocument();
+    expect(screen.getByLabelText("Correo Electrónico:")).toBeInTheDocument();
+    expect(screen.getByLabelText("Teléfono:")).toBeInTheDocument();
+    expect(screen.getByLabelText("Mensaje:")).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: "Enviar" })).toBeInTheDocument();
   });
 
-  it('Muestra mensajes de error cuando el formulario se envía con datos no válidos', async () => {
+  it("Muestra mensajes de error cuando el formulario se envía con datos no válidos", async () => {
     render(<App />);
 
     // Simular envío del formulario sin completar los campos requeridos
-    fireEvent.click(screen.getByRole('button', { name: 'Enviar' }));
+    fireEvent.click(screen.getByRole("button", { name: "Enviar" }));
 
     // Verificar que se muestren los mensajes de error
-    expect(await screen.findByText('El nombre es obligatorio')).toBeInTheDocument();
-    expect(await screen.findByText('El correo electrónico es obligatorio')).toBeInTheDocument();
-    expect(await screen.findByText('El numero de telefono es obligatorio')).toBeInTheDocument();
+    expect(
+      await screen.findByText("El nombre es obligatorio")
+    ).toBeInTheDocument();
+    expect(
+      await screen.findByText("El correo electrónico es obligatorio")
+    ).toBeInTheDocument();
+    expect(
+      await screen.findByText("El numero de telefono es obligatorio")
+    ).toBeInTheDocument();
   });
 
-  it('Envía el formulario cuando se ingresan datos válidos', async () => {
+  it("Envía el formulario cuando se ingresan datos válidos", async () => {
     render(<App />);
 
     // Completar los campos requeridos
-    fireEvent.change(screen.getByLabelText('Nombre Completo'), { target: { value: 'Yoselyn Vaquedano' } });
-    fireEvent.change(screen.getByLabelText('Correo Electrónico:'), { target: { value: 'yoselyn@gmail.com' } });
-    fireEvent.change(screen.getByLabelText('Teléfono:'), { target: { value: '123456789' } });
-    fireEvent.change(screen.getByLabelText('Mensaje:'), { target: { value: 'Hola, esto es un mensaje de prueba' } });
+    fireEvent.change(screen.getByLabelText("Nombre Completo"), {
+      target: { value: "Yoselyn Vaquedano" },
+    });
+    fireEvent.change(screen.getByLabelText("Correo Electrónico:"), {
+      target: { value: "yoselyn@gmail.com" },
+    });
+    fireEvent.change(screen.getByLabelText("Teléfono:"), {
+      target: { value: "123456789" },
+    });
+    fireEvent.change(screen.getByLabelText("Mensaje:"), {
+      target: { value: "Hola, esto es un mensaje de prueba" },
+    });
 
     // Simular envío del formulario
-    fireEvent.click(screen.getByRole('button', { name: 'Enviar' }));
+    fireEvent.click(screen.getByRole("button", { name: "Enviar" }));
 
     // Verificar que se muestre el mensaje de éxito
-    expect(await screen.findByText('¡Envío del formulario exitoso!')).toBeInTheDocument();
+    expect(
+      await screen.findByText("¡Envío del formulario exitoso!")
+    ).toBeInTheDocument();
   });
 });
